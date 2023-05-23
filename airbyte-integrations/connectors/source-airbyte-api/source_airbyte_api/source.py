d0611b6c73eabcf519eded9cecd41f74708d5726
lrdsc <louis.rousselot-de-saint-ceran@artefact.com>
2023-05-23 15:18:22 +0200

    basic features for requirements

--- airbyte-integrations/connectors/source-airbyte-api/source_airbyte_api/source.py
+++ airbyte-integrations/connectors/source-airbyte-api/source_airbyte_api/source.py
@@ -49,74 +49,13 @@ class AirbyteApiStream(HttpStream, ABC):
     def request_headers(self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None) -> Mapping[str, Any]:
         return {"user-agent": "source-airbyte-api"}
 
-# Basic incremental stream
-class IncrementalAirbyteApiStream(AirbyteApiStream, ABC):
-    """
-    TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
-         if you do not need to implement incremental sync for any streams, remove this class.
-    """
-
-    # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
-    state_checkpoint_interval = None
-
-    @property
-    def cursor_field(self) -> str:
-        """
-        TODO
-        Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
-        usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
-
-        :return str: The name of the cursor field.
-        """
-        return []
-
-    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
-        """
-        Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
-        the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
-        """
-        return {}
-
-
-class Employees(IncrementalAirbyteApiStream):
-    """
-    TODO: Change class name to match the table/data source this stream corresponds to.
-    """
-
-    # TODO: Fill in the cursor_field. Required.
-    cursor_field = "start_date"
-
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "employee_id"
-
-    def path(self, **kwargs) -> str:
-        """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/employees then this should
-        return "single". Required.
-        """
-        return "employees"
-
-    def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
-        """
-        TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
-
-        Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
-        This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
-        section of the docs for more information.
-
-        The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
-        necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
-        This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
-
-        An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
-        craft that specific request.
-
-        For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
-        this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
-        till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
-        the date query param.
-        """
-        raise NotImplementedError("Implement stream slices or delete this method!")
+class AirbyteApiSubStream(AirbyteApiStream,HttpSubStream):
+    
+    def request_params(self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None) -> MutableMapping[str, Any]:
+        base_dict =  super().request_params(stream_state, stream_slice, next_page_token)
+        
+        base_dict[self.parent.primary_key]=stream_slice["parent"][self.parent.primary_key]
+        return base_dict
 
 class Workspaces(AirbyteApiStream):
     primary_key = "workspaceId"
@@ -124,18 +63,26 @@ class Workspaces(AirbyteApiStream):
     def path(self, **kwargs) -> str:
         return "workspaces"
 
-class Sources(AirbyteApiStream, HttpSubStream):
+class Sources(AirbyteApiSubStream):
     primary_key = "sourceId"
 
     def path(self, **kwargs)-> str:
         return "sources"
 
-    def request_params(self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None) -> MutableMapping[str, Any]:
-        print(stream_slice)
-        base_dict =  super().request_params(stream_state, stream_slice, next_page_token)
-        base_dict["workspaceId"]=stream_slice["parent"]["workspaceId"]
-        return base_dict
         
+class Connections(AirbyteApiSubStream):
+    primary_key = "connectionId"
+
+    def path(self, **kwargs)-> str:
+        return "connections"
+
+
+class Jobs(AirbyteApiSubStream):
+    primary_key = "jobId"
+
+    def path(self, **kwargs)->str:
+        return "jobs"
+    
 class SourceAirbyteApi(AbstractSource):
     def check_connection(self, logger, config) -> Tuple[bool, any]:
         headers = {
@@ -153,4 +100,7 @@ class SourceAirbyteApi(AbstractSource):
         auth = TokenAuthenticator(token=config.get("apiKey")) 
         workspaces = Workspaces(authenticator=auth)
         sources = Sources(parent = workspaces, authenticator=auth)
-        return [workspaces, sources]
+        connections = Connections(parent = workspaces, authenticator=auth)
+        jobs = Jobs(parent=connections, authenticator=auth)
+
+        return [workspaces, sources,connections,jobs]
