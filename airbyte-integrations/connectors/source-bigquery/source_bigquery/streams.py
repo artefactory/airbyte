659b94da2d3ab53221a2a2a3c7b6666bd94bdbfa
Mariam <mariam@FRART0402M.local>
2024-04-26 13:06:22 +0200

    clean code

--- airbyte-integrations/connectors/source-bigquery/source_bigquery/streams.py
+++ airbyte-integrations/connectors/source-bigquery/source_bigquery/streams.py
@@ -73,8 +73,6 @@ class BigqueryStream(HttpStream, ABC):
             yield {
                 "_bigquery_table_id": record.get("tableReference")["tableId"],
                 "_bigquery_created_time": record.get("creationTime"),
-                "_airbyte_raw_id": uuid.uuid4(),
-                "_airbyte_extracted_at": datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S"), # TODO: check if correct format
                 **{element["name"]: rows[fields.index(element)]["v"] for element in fields},
             }
 
