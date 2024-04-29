042467691fb3e8dea1591a67971269d2654bf5b8
Mariam <mariam@FRART0402M.local>
2024-04-29 16:39:11 +0200

    Fix bug in SourceBigquery check_connection when a dataset has no tables

--- airbyte-integrations/connectors/source-bigquery/source_bigquery/streams.py
+++ airbyte-integrations/connectors/source-bigquery/source_bigquery/streams.py
@@ -124,9 +124,12 @@ class BigqueryTables(BigqueryDatasets):
         Override this method to define how a response is parsed.
         :return an iterable containing each record in the response
         """
-        tables = response.json().get(self.name, [])
-        for table in tables:
-            yield table
+        try:
+            tables = response.json().get(self.name)
+            for table in tables:
+                yield table
+        except TypeError as e:
+            self.logger.warning(f"Dataset has no tables causing the error {str(e)}")
 
 
 class BigqueryTable(BigqueryTables):
