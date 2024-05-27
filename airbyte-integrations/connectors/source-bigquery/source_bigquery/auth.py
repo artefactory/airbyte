64bb6e2b828716efe7abe908d753aaaeacb96f0a
Mariam <mariam.ahhttouche@artefact.com>
2024-05-27 17:04:30 +0200

    Bigquery source: Add BigqueryOAuth and fresh access token

--- airbyte-integrations/connectors/source-bigquery/source_bigquery/auth.py
+++ airbyte-integrations/connectors/source-bigquery/source_bigquery/auth.py
@@ -16,17 +16,38 @@ from airbyte_cdk.sources.streams.http.requests_native_auth import (
 
 SCOPES = ['https://www.googleapis.com/auth/bigquery', 'https://www.googleapis.com/auth/cloud-platform', 'https://www.googleapis.com/auth/cloud-platform.read-only']
 
+class BigqueryOAuth(TokenAuthenticator):
+    """
+    """
+    def __init__(self, config):
+        self._connector_config = config
+        credentials_json = json.loads(config["credentials_json"], strict=False)
+        self._credentials = ServiceAccountCredentials(credentials_json["client_email"], RSASigner.from_string(credentials_json["private_key"], \
+                                                        credentials_json["private_key_id"]),\
+                                                        scopes=SCOPES, \
+                                                        private_key_id=credentials_json["private_key_id"], client_id=credentials_json["client_id"])
+        h = httplib2.Http()
+        self._credentials.authorize(h)
+        self._token = str(self._credentials.get_access_token().access_token)
+        super().__init__(self._token)
+
+    @property
+    def token(self) -> str:
+        return f"{self._auth_method} {self.get_access_token()}"
+    
+    def get_access_token(self) -> str:
+        """Retrieve new access and refresh token if the access token has expired.
+        Returns:
+            str: The current access_token, updated if it was previously expired.
+        """
+        if self._credentials.access_token_expired:
+            self._credentials.refresh()
+            self._token = str(self._credentials.get_access_token().access_token)
+            # TODO: emit airbyte_cdk.sources.message
+        return self._token
+    
 
 class BigqueryAuth:
-    def __new__(cls, config: dict) -> Union[ServiceAccountCredentials, Oauth2Authenticator, TokenAuthenticator]:       
-        credentials_json = json.loads(config["credentials_json"], strict=False)
-
-        credentials = ServiceAccountCredentials(credentials_json["client_email"], RSASigner.from_string(credentials_json["private_key"], credentials_json["private_key_id"]),\
-                                                scopes=SCOPES, \
-                                                private_key_id=credentials_json["private_key_id"], client_id=credentials_json["client_id"])
-        h = httplib2.Http()
-        credentials.authorize(h)
-        token = credentials.get_access_token()
-        
-        return TokenAuthenticator(token=str(token.access_token))
+    def __new__(cls, config: dict) -> Union[ServiceAccountCredentials, Oauth2Authenticator, TokenAuthenticator]:
+        return BigqueryOAuth(config)
     
