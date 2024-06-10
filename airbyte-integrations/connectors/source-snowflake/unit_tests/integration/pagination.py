bb6e280c04048b8889269bd3b7771382c121986f
Louis Rousselot de Saint Ceran <louis.rousselot-de-saint-ceran@artefact.com>
2024-06-10 11:33:46 +0200

    test pagination

--- airbyte-integrations/connectors/source-snowflake/unit_tests/integration/pagination.py
+++ airbyte-integrations/connectors/source-snowflake/unit_tests/integration/pagination.py
@@ -1,14 +1,3 @@
-from airbyte_cdk.test.entrypoint_wrapper import EntrypointOutput, read
-from airbyte_cdk.test.mock_http import HttpMocker, HttpResponse
-from airbyte_cdk.test.mock_http.response_builder import (
-    FieldPath,
-    HttpResponseBuilder,
-    RecordBuilder,
-    create_record_builder,
-    create_response_builder,
-    find_template,
-)
-
 from typing import Any, Dict
 
 from airbyte_cdk.test.mock_http.response_builder import PaginationStrategy
@@ -17,6 +6,18 @@ from airbyte_cdk.test.mock_http.response_builder import PaginationStrategy
 class SnowflakePaginationStrategy(PaginationStrategy):
     @staticmethod
     def update(response: Dict[str, Any]) -> None:
-        response["partitionInfo"] = [1,2]
+        response["resultSetMetaData"]["partitionInfo"] = [
+            {
+            "rowCount": 12344,
+            "uncompressedSize": 14384873,
+            },{
+            "rowCount": 47387,
+            "uncompressedSize": 76483423,
+            "compressedSize": 4342748
+            },{
+            "rowCount": 43746,
+            "uncompressedSize": 43748274,
+            "compressedSize": 746323
+            }]
 
 
