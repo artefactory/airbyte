cbf7545acaed2aa21307d93d5a28ecfbff50b586
Hadrien DAURES <hadrien.daures@artefact.com>
2024-06-17 08:46:48 +0200

    Bigquery source: test#16 -- added some documentation on the BQ mocking helper

--- airbyte-integrations/connectors/source-bigquery/unit_tests/integration/mocked.py
+++ airbyte-integrations/connectors/source-bigquery/unit_tests/integration/mocked.py
@@ -5,6 +5,30 @@ from integration.bq_query_builder import build_query
 
 
 def mock_discover_calls(http_mocker, tables: Mapping[str, Mapping[str, Set[str]]]) -> None:
+    """
+    Mocks the calls to the BigQuery API to discover the tables in the project.
+
+    :param http_mocker: The HttpMocker instance to use for mocking the calls.
+    :param tables: A mapping from project_id to a mapping from dataset_id to a set of table_ids.
+
+    Example:
+    {
+        "project_id_1": {
+            "dataset_id_1": {
+                "table_id_1",
+                "table_id_2",
+            },
+            "dataset_id_2": {
+                "table_id_3",
+            },
+        },
+        "project_id_2": {
+            "dataset_id_3": {
+                "table_id_4",
+            },
+        },
+    }
+    """
     for project_id, datasets in tables.items():
         http_mocker.get(
             BigqueryRequestBuilder.datasets_endpoint(project_id=project_id).with_max_results(10000).build(),
